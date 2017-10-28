f2cb865395cdb27f83139275d8f8c1148b4ca937
Fritz Jay <jonathan.jay10@gmail.com>
2017-10-28 15:40:19 -0700

    Disabled cors

--- listener.py
+++ listener.py
@@ -1,14 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from sqlalchemy import create_engine
 from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 class Start_Test_Suite(Resource):
+    """ Starts the test suite """
     def get(self, test_suite_name):
-        return {'message': 'Started a Test Suite: %s' % test_suite_name}
+        return
 
 api.add_resource(Start_Test_Suite, '/start-test-suite/<string:test_suite_name>')
 
