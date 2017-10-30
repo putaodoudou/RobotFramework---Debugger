de752041970716d827937525eac414f03e769d9d
Fritz Jay <Jonathan.jay10@gmail.com>
2017-10-29 21:47:21 -0700

    starting over on the listener

--- listener/listener.py
+++ listener/listener.py
@@ -1,71 +1,40 @@
-from robot.running.model import TestSuite
-import threading
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from Queue import Queue
 import json
-import urlparse
-from queue import Queue
-import cgi
-
-commands = Queue()
-suites = None
-suites_result = None
-tests = []
-tests_results = []
-keywords = []
-keywords_result = []
-variables = []
-
-current_suite = None
-current_suite_result = None
-current_test = None
-current_test_result
-current_keyword = None
-current_keyword_result = None
+from robot.running.model import *
 
 class listener():
-    ROBOT_LISTENER_API_VERSION = 2
+    ROBOT_LISTENER_API_VERSION = 3
 
     def __init__(self):
-        print 'Listening on Port 3002'
-        addr = ('localhost', 3002)
-        self.server = HTTPServer(addr, RequestHandler)
-        thread = threading.Thread(target = self.server.serve_forever)
-        thread.daemon = True
-        thread.start()
+        commands = Queue()
 
     def start_suite(self, suite, result):
-        global commands
-        global current_suit
-        global current_suit_result
+        print 'Start Suite'
+        suite_json = json.dumps(suite)
+        print suite_json
+        result_json = json.dumps(result)
+        print result_json
 
-        self._clear_currents('suite')
+    def end_suite(self, suite, result):
+        print 'End Suite'
+        suite_json = json.dumps(suite)
+        print suite_json
+        result_json = json.dumps(result)
+        print result_json
 
-        if not commands.empty():
-            for command in commands:
-                if command.type == 'add_keyword':
-                    suite.tests.pop().keywords.create(command.args, command.kwargs)
+    def start_test(self, test, result):
+        print 'Start Test'
+        keywords = json.dumps(test.keywords)
+        print keywords
+        test_result = json.dumps(result)
+        print test_result
 
-        current_suit = suite
-        current_suit_result = result
-
-    def end_suite(self, data, result):
-        #suite = json.jsonify([data, result])
-        print 'end_suite'
-        if not self.server:
-            return
-        self.server.shutdown()
-
-    def start_test(self, name, attrs):
-        pass
-
-    def end_test(self, name, attrs):
-        pass
-
-    def start_keyword(self, name, attrs):
-        pass
-
-    def end_keyword(self, name, attrs):
-        pass
+    def end_test(self, test, result):
+        print 'End Test'
+        test_json = json.dumps(test)
+        print test_json
+        test_result = json.dumps(result)
+        print test_result
 
     def message(self, message):
         pass
@@ -82,78 +51,8 @@ class listener():
     def report_file(self, path):
         pass
 
-    def summary_file(self, path):
-        pass
-
     def debug_file(self, path):
         pass
 
-    def _clear_currents(self, command_type):
-        global current_suite
-        global current_suite_result
-        global current_test
-        global current_test_result 
-        global current_keyword
-        global current_keyword_result
-
-        if command_type == 'suite':
-            current_suite = None
-            current_suite_result = None
-            current_test = None
-            current_test_result = None
-            current_keyword = None
-            current_keyword_result = None
-
-        elif command_type == 'test':
-            current_test = None
-            current_test_result = None
-            current_keyword = None
-            current_keyword_result = None
-
-        #TODO keywords and stuff
-
-    
-class RequestHandler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-    def do_GET(self):
-        global suites
-        global suites_result
-
-        self._set_headers()
-        parsed_path = urlparse.urlparse(self.path)
-
-        if parsed_path.path == '/update':
-            response = json.dumps([suites, suites_result])
-            self.wfile.write(response)
-
-        if parsed_path.path == '/pause':
-            pass
-
-        if parsed_path.path == '/stop':
-            pass
-    
-    def do_POST(self):
-        global suites
-        global suites_result
-        global commands
-
-        self._set_headers()
-        parsed_path = urlparse.urlparse(self.path)
-        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-
-        self.send_response(200)
-        self.end_headers()
-
-        data = json.loads(self.data_string)
-        print data
-
-        if parsed_path.path == '/keyword':
-            global commands
-            commands.put(data)
-
-    def do_HEAD(self):
-        self._set_headers()
+    def close(self):
+        pass
